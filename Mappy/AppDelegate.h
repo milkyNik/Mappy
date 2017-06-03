3b8625313e510308810a3826747844d6a8d35b17
MilkyNik <milniklaus@gmail.com>
2017-06-03 23:38:03 +0300

    Initial Commit

---
+++ Mappy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Mappy
+//
+//  Created by iMac on 03.06.17.
+//  Copyright © 2017 hata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Mappy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Mappy
+//
+//  Created by iMac on 03.06.17.
+//  Copyright © 2017 hata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
